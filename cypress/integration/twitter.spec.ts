544ac06fae9dd40c011147eee58dedfcff2e3f8e
Adrian Widlak <awidlak@adrianwii.pl>
2021-05-21 09:32:20 +0200

    Write test with console log

---
+++ cypress/integration/twitter.spec.ts
@@ -0,0 +1,22 @@
+describe('should visit page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/');
+    cy.viewport(300, 600);
+  });
+
+  it('should visit main page', () => {
+    cy.contains('twitter works!');
+  });
+
+  it('should click button', () => {
+    cy.get('#clicker').click();
+  });
+
+  it('should add tweet', () => {
+
+  });
+
+  it('should test sth else', () => {
+      console.log('it very important test');
+  });
+});
